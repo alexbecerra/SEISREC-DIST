08bbd2ebf97a758d9dee8c56620404351db0e835
Ignacio Maldonado <revisiontecnica@gmail.com>
2020-04-28 11:30:23 -0400

    Minor changes

--- scripts/dist2dev.sh
+++ scripts/dist2dev.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 if [ -z "$repodir" ]; then
-    repodir="$HOME/SEISREC/"
+    repodir=$(sudo find -P "$HOME" -name "SEISREC-DIST")
 fi
 
 currdir=$(pwd)
 
-cd "$HOME/SEISREC-DIST/"
+if ! cd "$repodir"; then
+  printf "Error cd'ing into ./SEISREC-DEV!\n"
+  exit 1
+fi
 
 if ! git clone https://github.com/alexbecerra/SEISREC-DEV.git; then
   printf "Error cloning into ./SEISREC-DEV!\n"
   exit 1
 fi
 
-cd $currdir
+if ! cd $currdir; then
+  printf "Error cd'ing back into %s!\n" "$currdir"
+  exit 1
+fi
