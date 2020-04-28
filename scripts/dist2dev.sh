07003d6cf196dbf0ae43b23ccdbf109437c0aece
Ignacio Maldonado <revisiontecnica@gmail.com>
2020-04-28 11:33:05 -0400

    Code Cleanup

--- scripts/dist2dev.sh
+++ scripts/dist2dev.sh
@@ -2,20 +2,27 @@
 
 if [ -z "$repodir" ]; then
     repodir=$(sudo find -P "$HOME" -name "SEISREC-DIST")
+    if [ -z "$repodir" ]; then
+      printf "Error finding repo directory!\n"
+      exit 1
+    fi
 fi
 
 currdir=$(pwd)
+printf "cd'ing into %s" "$repodir"
 
 if ! cd "$repodir"; then
   printf "Error cd'ing into ./SEISREC-DEV!\n"
   exit 1
 fi
 
+printf "Cloning SEISREC-DEV...\n"
 if ! git clone https://github.com/alexbecerra/SEISREC-DEV.git; then
   printf "Error cloning into ./SEISREC-DEV!\n"
   exit 1
 fi
 
+printf "cd'ing cd'ing back into %s!\n" "$currdir"
 if ! cd $currdir; then
   printf "Error cd'ing back into %s!\n" "$currdir"
   exit 1
