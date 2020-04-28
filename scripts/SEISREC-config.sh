2f8f8f37e07bee2da791e0eb4b61eda727fd3bfa
Ignacio Maldonado <revisiontecnica@gmail.com>
2020-04-27 22:59:24 -0400

    Skeletonized program flow

--- scripts/SEISREC-config.sh
+++ scripts/SEISREC-config.sh
@@ -1,90 +1,112 @@
 #!/bin/bash
 
-param=''
-editsingle=''
-view=''
+debug=''
+choice=""
+done=""
+function print_help() {
+  printf "\n"
+  printf "\n"
+  printf "\n"
+  printf "\n"
+  printf "\n"
+  printf "\n"
+  printf "\n"
+  exit 0
+}
 
 # Parse options
-while getopts "p:vh" opt; do
+while getopts "dh" opt; do
   case ${opt} in
-    p )
-      editsingle="yes"
-      param="$OPTARG"
-      ;;
-    v )
-      view='yes'
-      ;;
-    h )
-      echo "Usage: SEISREC-config.sh [options]"
-      echo "    [-h]                  Display this help message & exit."
-      echo "    [-p] <parameter>      Edit single parameter from configuration file"
-      echo "    [-v]                  View config file and exit"
-      exit 0
-      ;;
-   \? )
-     echo "Invalid Option: -$OPTARG" 1>&2
-     exit 1
-     ;;
+  d)
+    debug="yes"
+    ;;
+  h)
+    printf "Usage: SEISREC-config.sh [options]"
+    printf "    [-h]                  Display this help message & exit.\n"
+    printf "    [-d]                  Enable debug messages.\n"
+    exit 0
+    ;;
+  \?)
+    printf "Invalid Option: -%s" "$OPTARG" 1>&2
+    exit 1
+    ;;
   esac
 done
-shift $((OPTIND -1))
+shift $((OPTIND - 1))
 
-PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
-select opt in "${options[@]}"
-do
+printf "SEISREC_config.sh - SEISREC configuration & setup utility\n"
+
+if [ -z "$repodir" ]; then
+  repodir="$HOME"
+fi
+
+while [ -z "$done" ]; do
+  PS3='Selection: '
+  options=("Configure Station" "Station Setup" "Help" "Quit")
+  select opt in "${options[@]}"; do
     case $opt in
-        "Option 1")
-            echo "you chose choice 1"
-            ;;
-        "Option 2")
-            echo "you chose choice 2"
-            ;;
-        "Option 3")
-            echo "you chose choice $REPLY which is $opt"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
+    "Configure Station")
+      choice="${options[0]}"
+      break
+      ;;
+    "Station Setup")
+      choice="${options[1]}"
+      break
+      ;;
+    "Help")
+      print_help
+      ;;
+    "Quit")
+      printf "Good bye!\n"
+      exit 0
+      ;;
+    *) printf "invalid option %s\n" "$REPLY" ;;
     esac
-done
+  done
 
-currdir=`pwd`
-
-cd ~/SEISREC/
-
-if [ ! -z "$view" ]
-then
-  cat parameter
-  exit 0
-fi
-
-if [ ! -z "$editsingle" ]
-then
-  ~/SEISREC/build/bin/param-edit -pth ~/SEISREC/ -param $param
-  exit 0
-fi
-
-~/SEISREC/build/bin/param-edit -pth ~/SEISREC/
-
-# if symlink to SEISREC-config doesn't exist, create it
-if [ ! -h "$repodir/SEISREC-DIST/SEISREC-config" ]; then
-  printf "Creating symlinks to SEISREC-config...\n"
-  ln -s "$repodir/SEISREC-DIST/scripts/SEISREC-config.sh" "$repodir/SEISREC-DIST/SEISREC-config"
-fi
-
-# Check if ~/SEISREC is in PATH, if not, add it to PATH
-inBashrc=$(cat "$HOME/.bashrc" | grep 'SEISREC-DIST')
-inPath=$(printf "%s" "$PATH" | grep 'SEISREC-DIST')
-if [ -z "$inBashrc" ]; then
-  if [ -z "$inPath" ]; then
-    # Add it permanently to path
-    printf "Adding ./SEISREC-DIST to PATH...\n"
-    printf 'inPath=$(printf "$PATH"|grep "SEISREC-DIST")\n' >>~/.bashrc
-    printf 'if [ -z "$inPath" ]\n' >>~/.bashrc
-    printf 'then\n' >>~/.bashrc
-    printf '  export PATH="~/SEISREC-DIST:$PATH"\n' >>~/.bashrc
-    printf 'fi\n' >>~/.bashrc
+  if [ -n "$debug" ]; then
+    printf "choice = %s\n" "$choice"
   fi
-fi
+
+  case $choice in
+  "Configure Station")
+  if [ -f "$repodir/SEISREC-DIST/parameter" ]; then
+    printf "No parameter file found! Please run station setup first!\n"
+  else
+    "$repodir/SEISREC-DIST/util/param-edit" -pth "$repodir/SEISREC-DIST/"
+  fi
+  ;;
+
+  "Station Setup")
+
+
+  "$repodir/SEISREC-DIST/scripts/install_services.sh INSTALL"
+
+
+  if [ -n "$cfgeverywhere" ]; then
+  # if symlink to SEISREC-config doesn't exist, create it
+  if [ ! -h "$repodir/SEISREC-DIST/SEISREC-config" ]; then
+    printf "Creating symlinks to SEISREC-config...\n"
+    ln -s "$repodir/SEISREC-DIST/scripts/SEISREC-config.sh" "$repodir/SEISREC-DIST/SEISREC-config"
+  fi
+
+  # Check if ~/SEISREC is in PATH, if not, add it to PATH
+  inBashrc=$(cat "$HOME/.bashrc" | grep 'SEISREC-DIST')
+  inPath=$(printf "%s" "$PATH" | grep 'SEISREC-DIST')
+  if [ -z "$inBashrc" ]; then
+    if [ -z "$inPath" ]; then
+      # Add it permanently to path
+      printf "Adding ./SEISREC-DIST to PATH...\n"
+      printf 'inPath=$(printf "$PATH"|grep "SEISREC-DIST")\n' >>~/.bashrc
+      printf 'if [ -z "$inPath" ]\n' >>~/.bashrc
+      printf 'then\n' >>~/.bashrc
+      printf '  export PATH="~/SEISREC-DIST:$PATH"\n' >>~/.bashrc
+      printf 'fi\n' >>~/.bashrc
+    fi
+  fi
+  fi
+  ;;
+  esac
+done
+printf "Good bye!\n"
+exit 0
