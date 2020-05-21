be429ce51bf2d50664e79bec7d5e563cdfdd112b
Ignacio Maldonado <revisiontecnica@gmail.com>
2020-05-21 12:25:17 -0400

    Typo Fix

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -206,7 +206,7 @@ if [ -n "$install" ]; then
       if [ -z "$(ls "$repodir/SEISREC-DIST/unit/" | grep "$unitname")" ]; then
         printf "No corresponding unit executable for %s!!\n" "$f"
       elif [ "$repodir" != "/home/pi" ]; then
-        if ! sed -i "s*home/pi*$repodir*" "$workdir/services/$f"; then
+        if ! sed -i "s*home/pi*$repodir*" "$repodir/SEISREC-DIST/services/$f"; then
           printf "Error setting unit executable paths in %s\n!" "$f"
         fi
       fi
